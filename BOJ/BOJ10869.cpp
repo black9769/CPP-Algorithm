841f37bc68fede01d525f7b09203d57185b8b510
black9769 <black9769@naver.com>
2024-08-05 21:39:07 +0900

    [feat] BOJ10869

---
+++ BOJ/BOJ10869.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int a, b = 0;
+
+	cin >> a >> b;
+
+	cout << a+b <<endl;
+	cout << a - b <<endl;
+	cout << a * b <<endl;
+	cout <<a/b <<endl;
+	cout <<a%b <<endl;
+	
+	return 0;
+}
